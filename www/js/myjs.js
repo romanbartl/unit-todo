8a976b5f102c09e23e32a4be57b48965dba7a713
B4rtosek <B4rtosek@gmail.com>
2018-04-07 01:12:35 +0200

    plan and mybag changes

--- www/js/myjs.js
+++ www/js/myjs.js
@@ -7,3 +7,33 @@ $(document).ready(function () {
         }, 500);
     });
 });
+
+var count = 0;
+function addAnotherBar() {
+    if (count < 5){
+        document.getElementById('dylytko').style.display = 'block';
+        count = count + 1;
+        $( ".ready-to-add" ).append( "<div id='"+count+"' class=\"another-bar\">\n" +
+            "        <li>\n" +
+            "            <p>ANOTHER BAR HERE</p>\n" +
+            "        </li></div>" );
+        if (count == 5){
+            document.getElementById('additko').style.display = 'none';
+        }
+    } else {
+        document.getElementById('additko').style.display = 'none';
+    }
+}
+
+function delAnotherBar() {
+    $("#"+count).remove();
+
+
+    count = count - 1;
+    if (count == 0){
+        document.getElementById('dylytko').style.display = 'none';
+    } else {
+        document.getElementById('additko').style.display = 'block';
+    }
+
+}
