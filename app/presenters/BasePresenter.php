82e3cc1558139e12f5d4da3a8983ac85971a5dcf
Roman Bartl <bbartl.roman@gmail.com>
2018-04-07 00:31:10 +0200

    another ajax

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -8,6 +8,7 @@ use Contributte\Facebook\FacebookLogin;
 use Nette\Application\Responses\RedirectResponse;
 use Nette\Application\UI\Presenter;
 use Nette\Security\AuthenticationException;
+use \Nette\Application\UI\Form;
 
 
 class BasePresenter extends \Nette\Application\UI\Presenter
@@ -18,56 +19,108 @@ class BasePresenter extends \Nette\Application\UI\Presenter
     /** @var UserFactory @inject */
     public $UserFactory;
 
-
-    public function handleFacebookCookie()
+    public function isLogged()
     {
-        try {
-            $token = $this->facebookLogin->getAccessTokenFromCookie();
-            $data = $this->facebookLogin->getMe($token, ['id', 'email']);
+        $user = $this->getUser();
 
-            if ($this->UserFactory->loginByFacebook($data['email'], $data['id'], $token))
-            {
-                $user = $this->getUser();
-                $user->login($data['email'], NULL);
-
-                $this->flashMessage('JOOOO', 'success');
-                $this->redrawControl('flashMessages');
-            }
-
-        } catch (FacebookLoginException | AuthenticationException $e) {
-            // TODO
-            $this->flashMessage($e->getMessage(), 'danger');
-            $this->redrawControl('flashMessages');
+        if ($user->isLoggedIn())
+        {
+            $this->redirect('Homepage:');
         }
     }
 
-    protected function createComponentRegistrationForm()
+    public function actionRegister()
+    {
+        $this->isLogged();
+    }
+
+    public function actionLogin()
+    {
+        $this->isLogged();
+    }
+
+    public function actionOut()
+    {
+        $user = $this->getUser();
+        $user->logOut();
+        $this->redirect('Homepage:');
+    }
+
+    public function handleFacebookCookie()
+    {
+        if($this->isAjax()) {
+            try {
+                $token = $this->facebookLogin->getAccessTokenFromCookie();
+                $data = $this->facebookLogin->getMe($token, ['id', 'email']);
+
+                if ($this->UserFactory->loginByFacebook($data['email'], $data['id'], $token)) {
+                    $user = $this->getUser();
+                    $user->login($data['email'], NULL);
+                }
+
+            } catch (FacebookLoginException | AuthenticationException $e) {
+                $this->flashMessage($e->getMessage(), 'danger');
+                $this->redrawControl('flashMessages');
+            }
+        }
+    }
+
+    protected function createComponentLoginForm()
     {
         $form = new \Nette\Application\UI\Form;
-        $form->getElementPrototype()->class('lol');
-        $form->getElementPrototype()->class('ajax');
 
-        $form->addText('email')->setAttribute('class', 'form-control')->setAttribute('placeholder', 'Email');
-        $form->addPassword('passwd')->setAttribute('class', 'form-control')->setAttribute('placeholder', 'Password');;
-        $form->addPassword('passwdAgain')->setAttribute('class', 'form-control')->setAttribute('placeholder', 'Password again');
+        $form->addEmail('email')->setAttribute('class', 'form-control')->setAttribute('placeholder', 'Email');
+        $form->addPassword('passwd')->setAttribute('class', 'form-control')->setAttribute('placeholder', 'Heslo');
 
-        $form->addSubmit('register', 'Registrovat')->setAttribute('class', 'btn btn-primary btn-block');
+        $form->addSubmit('login', 'Přihlásit')->setAttribute('class', 'btn btn-primary btn-block mojeButton');
         $form->onSuccess[] = [$this, 'registrationFormSucceeded'];
         return $form;
     }
 
-    public function registrationFormSucceeded()
-    {
-        if($this->isAjax()) {
 
-        }
+    protected function createComponentRegistrationForm()
+    {
+        $form = new \Nette\Application\UI\Form;
+        $form->getElementPrototype()->class('ajax');
+
+        $form->addEmail('email')->setAttribute('class', 'form-control')->setAttribute('placeholder', 'Email')
+            ->setRequired('Zadejde email');
+        $form->addPassword('passwd')->setAttribute('class', 'form-control')->setAttribute('placeholder', 'Heslo')
+            ->addRule(Form::PATTERN, 'Musí obsahovat číslici', '.*[0-9].*')
+            ->addRule(Form::PATTERN, 'Musí jedno velké písmeno', '.*[A-Z].*')
+            ->addRule(Form::PATTERN, 'Musí jedno malé písmeno', '.*[a-z].*')
+            ->setRequired('Zadejte heslo');
+        $form->addPassword('passwdAgain')->setAttribute('class', 'form-control')->setAttribute('placeholder', 'Heslo znovu')
+            ->addRule(Form::PATTERN, 'Musí obsahovat číslici', '.*[0-9].*')
+            ->addRule(Form::PATTERN, 'Musí jedno velké písmeno', '.*[A-Z].*')
+            ->addRule(Form::PATTERN, 'Musí jedno malé písmeno', '.*[a-z].*')
+            ->setRequired('Zadejte znovu heslo');
+
+        $form->addSubmit('register', 'Registrovat')->setAttribute('class', 'btn btn-primary btn-block mojeButton');
+        $form->onSuccess[] = array($this, 'registrationFormSucceeded');
+        return $form;
+    }
+
+    public function registrationFormSucceeded($form, $values)
+    {
+        //if($this->isAjax()) {
+            if (!$this->UserFactory->register($values['email'], $values['passwd'])) {
+                $this->flashMessage('Uživatel s tímto emailem již existuje', 'danger');
+                $this->redrawControl('flashMessages');
+            }
+        //}
     }
 
     public function renderRegister()
     {
         if($this->isAjax()) {
-            //$this->template->pes = "2";
-            //$this->redirect('Homepage:register');
+            $this->redrawControl('contentSnippet');
+        }
+    }
+
+    public function renderLogin()
+    {
+        if($this->isAjax()) {
             $this->redrawControl('contentSnippet');
         }
     }
